4bc1868cd4fab6ddaf3553b547410874270c0d3b
Diego Popritkin <diegopopritkin@gmail.com>
2024-06-07 12:55:55 -0300

    se agrega el preload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PreloadStrategyService } from './services/preload-strategy.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadStrategyService)),
+    provideAnimationsAsync('noop'),
+  ]
 };
