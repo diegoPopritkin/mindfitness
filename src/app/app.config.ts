91293c182dbba111f3ae7200989f7c865d213012
Diego Popritkin <diegopopritkin@gmail.com>
2024-06-12 19:59:11 -0300

    se agrega llamdo al back

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { PreloadStrategyService } from './services/preload-strategy.service';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withPreloading(PreloadStrategyService)),
     provideAnimationsAsync('noop'),
+    provideHttpClient(),
   ]
 };
