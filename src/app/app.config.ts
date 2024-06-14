4602da972dacc3521a611d0484f464589caa79cb
Diego Popritkin <diegopopritkin@gmail.com>
2024-06-14 13:46:26 -0300

    se agrega interceptor http para reintentar en caso de error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,14 @@ import { PreloadStrategyService } from './services/preload-strategy.service';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { retryInterceptor } from './interceptors/retry-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withPreloading(PreloadStrategyService)),
     provideAnimationsAsync('noop'), // disable animations
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([retryInterceptor]),),
   ]
 };
---
+++ src/app/interceptors/retry-interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { timer, retry } from "rxjs";
+
+export const retryInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  return next(req).pipe(retry({ count: 1, delay: shouldRetry }))
+
+  function shouldRetry(error: HttpErrorResponse) {
+    if (error.status >= 500) {
+      return timer(1000);
+    }
+    else {
+      throw error;
+    }
+  }
+}
