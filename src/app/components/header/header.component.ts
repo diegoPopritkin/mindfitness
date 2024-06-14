df99dafa21d204a56ba28256b5b3606edc90480b
Diego Popritkin <diegopopritkin@gmail.com>
2024-06-14 11:47:14 -0300

    se crea el componente personalizado reutilizable header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  goHome() {
+    this.router.navigate(['start']);
+  }
+}
--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, HeaderComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
