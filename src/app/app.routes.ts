1d5f451decc6215b91dffb1f9f49531f9f8f4c96
Diego Popritkin <diegopopritkin@gmail.com>
2024-06-14 09:18:04 -0300

    se agrega el ruteo lazy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'start',
+        loadComponent: () =>
+            import('./pages/start/start.component')
+                .then(m => m.StartComponent)
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'start'
+    },
+];
